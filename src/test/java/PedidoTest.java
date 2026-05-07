786aa5d6244ee6bab70c2456d3475251638664da
Geovane Oliveira <136372293+Jhonecle@users.noreply.github.com>
2026-05-06 22:17:34 -0300

    Renomeando o arquivo

--- src/test/java/PedidoTest.java
+++ src/test/java/PedidoTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PedidoTeste {
+public class PedidoTest {
 
     @Test
     public void deveCalcularTotalCorretamente() {
