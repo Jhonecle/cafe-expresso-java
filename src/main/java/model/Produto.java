c4b4b5d659d6447453b200d4951782e2dd745a13
Marcio Augusto <marcioacfernandes@gmail.com>
2026-05-07 19:47:35 -0300

    Atualização do arquivo Produto.java inserçao getset

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,25 +1,29 @@
 package model;
 
 public class Produto {
-    private String nome;
-    private double precoUnitario;
 
-    public Produto(String nome, double precoUnitario) {
-        if (nome == null || nome.isEmpty()) {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+
+        if (nome == null || nome.isBlank()) {
             throw new IllegalArgumentException("Nome inválido");
         }
-        if (precoUnitario <= 0) {
+
+        if (preco <= 0) {
             throw new IllegalArgumentException("Preço inválido");
         }
+
         this.nome = nome;
-        this.precoUnitario = precoUnitario;
-           }
+        this.preco = preco;
+    }
 
     public String getNome() {
         return nome;
     }
 
-    public double getPrecoUnitario() {
-        return precoUnitario;
+    public double getPreco() {
+        return preco;
     }
 }
